2d556dd7326db826493fde33198bb549f37710ee
erife <erifeoluwa@gmail.com>
2020-04-22 22:48:57 +0100

    declared an arraylist

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -4,12 +4,15 @@ import processing.core.PApplet;
 
 public class Gantt extends PApplet
 {	
+	//declaring an ArrayList to hold instances of the Task class
+	public ArrayList<Task> tasks = new ArrayList<Task>();
 	
 	public void settings()
 	{
 		size(800, 600);
 	}
 
+	
 	public void loadTasks()
 	{
 		
