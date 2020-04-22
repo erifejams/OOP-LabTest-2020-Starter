73d5d045e551a8b95e77dc1ed9d6b3e3c53653f7
erife <erifeoluwa@gmail.com>
2020-04-22 22:50:39 +0100

    wrote method loadTasks

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -1,22 +1,29 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
 import processing.core.PApplet;
-
+import processing.data.Table;
+import processing.data.TableRow;
 public class Gantt extends PApplet
 {	
 	//declaring an ArrayList to hold instances of the Task class
 	public ArrayList<Task> tasks = new ArrayList<Task>();
-	
+
 	public void settings()
 	{
 		size(800, 600);
 	}
 
-	
 	public void loadTasks()
 	{
-		
+		Table t = loadTable("tasks.csv", "header");
+		for(TableRow rows:t.rows())
+		{
+			Task ta = new Task(rows);
+			tasks.add(ta);
+		}
 	}
+
 
 	public void printTasks()
 	{
